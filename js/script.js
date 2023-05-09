1d4ca08eab685e8cbe66f5a3f2a69ef672bc2677
Ali <apollon.play15@gmail.com>
2023-05-10 01:45:30 +0400

    -_-

--- js/script.js
+++ js/script.js
@@ -13,3 +13,8 @@ const burger = document.querySelector('.burger'),
 burger.addEventListener('click', ()=>{
     menuList.classList.toggle('menu__list-active');
 });
+
+let buttonPlay = document.querySelector('.video-inner');
+buttonPlay.onclick = function() {
+  this.innerHTML = '<div><iframe class="video" width="1000px" height="500px" src="https://www.youtube.com/embed/lq149gCzkvE?autoplay=1" title="Youtube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>';
+};
